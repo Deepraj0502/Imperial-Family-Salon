faac2c66f0dc6dd6d3a1f02976d52e089cee97a6
DeEpRaJ <pagaredeepraj05@gmail.com>
2023-10-10 20:32:13 +0530

    Update Footer.js

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -65,7 +65,7 @@ const Footer = () => {
 						<ul class="box">
 							<li class="link_name">Address</li>
 							<li>
-							<p style={{color:"white",width:"300px"}}>Hariom Heritage CHS, Shop No.11, Sector 21, Kharghar, Navi Mumbai, Maharashtra 410210</p>
+							<p style={{color:"white",width:"300px"}}>Shop no -7, Krushna Kunj Building, Plot No.20, Sector-15, Kamothe, Navi Mumbai, Maharashtra 410209</p>
 							</li>
 						</ul>
 						<ul class="box">
@@ -91,7 +91,7 @@ const Footer = () => {
 				</div>
 				<iframe
 								title={Map}
-								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30173.066258200015!2d73.05549187661465!3d19.035874475086946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3fa1d15fea7%3A0xeaf7e831944bcf2d!2sImperial%20Family%20Salon!5e0!3m2!1sen!2sin!4v1671702151163!5m2!1sen!2sin"
+								src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15088.575871873943!2d73.0940921!3d19.0133767!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3fa1d15fea7%3A0xeaf7e831944bcf2d!2sImperial%20Family%20Salon!5e0!3m2!1sen!2sin!4v1696949987737!5m2!1sen!2sin"
 								width="600"
 								height="400"
 								frameborder="0"
@@ -172,14 +172,14 @@ const Footer = () => {
 						<ul class="box">
 							<li class="link_name">Address</li>
 							<li>
-							<p style={{color:"white",width:"300px",fontSize:"12px"}}>Hariom Heritage CHS, Shop No.11, Sector 21, Kharghar, Navi Mumbai, Maharashtra 410210</p>
+							<p style={{color:"white",width:"300px",fontSize:"12px"}}>Shop no -7, Krushna Kunj Building, Plot No.20, Sector-15, Kamothe, Navi Mumbai, Maharashtra 410209</p>
 							</li>
 						</ul>
 					</div>
 				</div>
 				<iframe
 								title={Map}
-								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30173.066258200015!2d73.05549187661465!3d19.035874475086946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3fa1d15fea7%3A0xeaf7e831944bcf2d!2sImperial%20Family%20Salon!5e0!3m2!1sen!2sin!4v1671702151163!5m2!1sen!2sin"
+								src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15088.575871873943!2d73.0940921!3d19.0133767!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3fa1d15fea7%3A0xeaf7e831944bcf2d!2sImperial%20Family%20Salon!5e0!3m2!1sen!2sin!4v1696949987737!5m2!1sen!2sin"
 								width="300"
 								height="200"
 								frameborder="0"
